8d6e9c474a769eb0c37d08bbdcae48df44011187
aysingha <Ayush.Singhal@silabs.com>
2022-07-04 18:34:26 +0530

    added new route

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {'data': {"message": "Hello "}}
+
+
+'''
+If we use dictionary in value part of data then it gets converted to list in JSON
+example: {'data':{'ayush','singhal'}} the {'ayush','singhal'} part gets converted to ['ayush','singhal']
+'''
+
+
+@app.get('/about')
+async def about():
+    return {'data': {'ayush', 'singhal'}}
 
 
 @app.get("/hello/{name}")
